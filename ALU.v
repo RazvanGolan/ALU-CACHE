835a333021c35367549284c27fe821f1130cafac
Razvan Bitea <razvanbitea@gmail.com>
2024-03-22 09:56:52 +0200

    merge ceva

--- ALU.v
+++ ALU.v
@@ -1,42 +1,77 @@
-module RippleCarryAdder (
-    input [15:0] operandA,
-    input [15:0] operandB,
-    output reg [15:0] result
-);
+module ripple_carry_16_bit(a, b, cin,sum, cout);
+input [15:0] a,b;
+input cin;
+output [15:0] sum;
+output cout;
+wire c1,c2,c3;
 
-    reg [15:0] carry; // Carry bits
+ripple_carry_4_bit rca1 (
+.a(a[3:0]),
+.b(b[3:0]),
+.cin(cin), 
+.sum(sum[3:0]),
+.cout(c1));
 
-    genvar i;
-    generate
-        for (i = 0; i < 16; i = i + 1) begin : ripple_adders
-            FullAdderCell adder_inst (
-                .operandA(operandA[i]),
-                .operandB(operandB[i]),
-                .carryIn(carry[i]),
-                .sum(result[i]),
-                .carryOut(carry[i+1])
-            );
-        end
-    endgenerate
+ripple_carry_4_bit rca2(
+.a(a[7:4]),
+.b(b[7:4]),
+.cin(c1),
+.sum(sum[7:4]),
+.cout(c2));
 
+ripple_carry_4_bit rca3(
+.a(a[11:8]),
+.b(b[11:8]),
+.cin(c2),
+.sum(sum[11:8]),
+.cout(c3));
+
+ripple_carry_4_bit rca4(
+.a(a[15:12]),
+.b(b[15:12]),
+.cin(c3),
+.sum(sum[15:12]),
+.cout(cout));
 endmodule
 
-module FullAdderCell (
-    input operandA,
-    input operandB,
-    input carryIn,
-    output reg sum,
-    output reg carryOut
-);
+////////////////////////////////////
+//4-bit Ripple Carry Adder
+////////////////////////////////////
 
-    always @* begin
-        sum = operandA ^ operandB ^ carryIn;
-        carryOut = (operandA & operandB) | (operandA & carryIn) | (operandB & carryIn);
-    end
+module ripple_carry_4_bit(a, b, cin, sum, cout);
+input [3:0] a,b;
+input cin;
+wire c1,c2,c3;
+output [3:0] sum;
+output cout;
 
+full_adder fa0(.a(a[0]), .b(b[0]),.cin(cin), .sum(sum[0]),.cout(c1));
+full_adder fa1(.a(a[1]), .b(b[1]), .cin(c1), .sum(sum[1]),.cout(c2));
+full_adder fa2(.a(a[2]), .b(b[2]), .cin(c2), .sum(sum[2]),.cout(c3));
+full_adder fa3(.a(a[3]), .b(b[3]), .cin(c3), .sum(sum[3]),.cout(cout));
 endmodule
 
+//////////////////////////////
+//1bit Full Adder
+/////////////////////////////
+module full_adder(a,b,cin,sum, cout);
+input a,b,cin;
+output sum, cout;
+wire x,y,z;
+half_adder h1(.a(a), .b(b), .sum(x), .cout(y));
+half_adder h2(.a(x), .b(cin), .sum(sum), .cout(z));
+or or_1(cout,z,y);
+endmodule
 
+///////////////////////////
+// 1 bit Half Adder
+//////////////////////////
+module half_adder( a,b, sum, cout );
+input a,b;
+output sum, cout;
+xor xor_1 (sum,a,b);
+and and_1 (cout,a,b);
+endmodule
 
 module ALU (
     input [15:0] operandA,
@@ -47,48 +82,52 @@ module ALU (
     output reg [31:0] result
 );
 
-// Registers
-reg [15:0] regA;
-reg [15:0] regB;
-
 // Adder
 wire [15:0] add_result;
 
-// Instantiate RippleCarryAdder module for addition
-RippleCarryAdder adder_inst (
-    .operandA(operandA),
-    .operandB(operandB),
-    .result(add_result)
+// Instantiate RippleCarry16Bit module for addition
+ripple_carry_16_bit adder_inst (
+    .a(operandA),
+    .b(operandB),
+    .cin(1'b0), // Set carry-in based on opcode (0 for addition, 1 for subtraction)
+    .sum(add_result),
+    .cout()
 );
 
-// Substraction
+// Subtraction
 wire [15:0] sub_result;
 
-// Instantiate RippleCarryAdder module for substraction
-RippleCarryAdder substractor_adder_inst (
-    .operandA(operandA),
-    .operandB(~operandB + 1'b1),
-    .result(sub_result)
+// Instantiate RippleCarry16Bit module for subtraction
+ripple_carry_16_bit subtractor_inst (
+    .a(operandA),
+    .b(~operandB + opcode[0]), // Two's complement of operandB + carry-in
+    .cin(1'b0), // Set carry-in based on opcode (0 for addition, 1 for subtraction)
+    .sum(sub_result),
+    .cout()
 );
 
 // Increment
 wire [15:0] increment;
 
 // Instantiate RippleCarryAdder module for Increment
-RippleCarryAdder increment_adder_inst (
-    .operandA(operandA),
-    .operandB(1'b1),
-    .result(increment)
+ripple_carry_16_bit increment_inst (
+    .a(operandA),
+    .b({15'b0, 1'b1}),
+    .cin(1'b0),
+    .sum(increment),
+    .cout()
 );
 
 // Decrement
 wire [15:0] decrement;
 
 // Instantiate RippleCarryAdder module for Decrement
-RippleCarryAdder decrement_adder_inst (
-    .operandA(operandA),
-    .operandB(16'b1111_1111_1111_1111), // minus 1 in two's complement on 16 bits
-    .result(sub_result)
+ripple_carry_16_bit decrement_inst (
+    .a(operandA),
+    .b(16'b1111_1111_1111_1111), // minus 1 in two's complement on 16 bits
+    .cin(1'b0),
+    .sum(decrement),
+    .cout()
 );
 
 
@@ -104,57 +143,46 @@ reg [31:0] mux_output;
 // Control unit
 always @(posedge clk or posedge reset) begin
     if (reset) begin
-        regA <= 0;
-        regB <= 0;
         result <= 0;
     end
     else begin
         case(opcode)
-            // 4'b0000: begin // Load operands
-            //     regA <= operandA;
-            //     regB <= operandB;
-            // end
-            4'b0001: begin // Add
-                mux_output <= {16'b0, add_result};
+            4'b0000: begin // Add
+                result = add_result;
             end
-            4'b0010: begin // Substract
-                mux_output <= {16'b0, sub_result};
+            4'b0001: begin // Substract
+                result = sub_result;
             end
-            4'b0011: begin // Multiply
-                mult_result <= regA * regB;
-                mux_output <= mult_result[31:0]; // Considering only lower 32 bits of the result
+            4'b0010: begin // Multiply
+                mult_result <= operandA * operandB;
+                result <= mult_result[31:0]; // Considering only lower 32 bits of the result
             end
-            4'b0100: begin // Division
-                div_result <= regA / regB;
-                mux_output <= div_result[15:0]; 
+            4'b0011: begin // Division
+                div_result <= operandA / operandB;
+                result <= div_result[15:0]; 
             end
-            4'b0101: begin // AND
-                mux_output <= regA & regB;
+            4'b0100: begin // AND
+                result <= operandA & operandB;
             end
-            4'b0110: begin // OR
-                mux_output <= regA | regB;
+            4'b0101: begin // OR
+                result <= operandA | operandB;
             end
-            4'b0111: begin // XOR
-                mux_output <= regA ^ regB;
+            4'b0110: begin // XOR
+                result <= operandA ^ operandB;
             end
-            4'b1000: begin // LLS
-                mux_output <= regA << regB;
+            4'b0111: begin // LLS
+                result <= operandA << operandB;
             end
-            4'b1001: begin // LRS
-                mux_output <= regA >> regB;
+            4'b1000: begin // LRS
+                result <= operandA >> operandB;
             end
-            4'b1010: begin // Increment
-                mux_output <= increment;
+            4'b1001: begin // Increment
+                result <= increment;
             end
-            4'b1011: begin // Decrement
-                mux_output <= decrement;
+            4'b1010: begin // Decrement
+                result <= decrement;
             end
-            default: mux_output <= 0; // Default behavior
-        endcase
-        // Mux for selecting between adder and multiplier output
-        case(opcode)
-            4'b0001, 4'b0010, 4'b0011, 4'b0100, 4'b0101, 4'b0110, 4'b0111, 4'b1000, 4'b1001, 4'b1010, 4'b1011 : result <= mux_output;
-            default: result <= 0;
+            default: result <= 0; // Default behavior
         endcase
     end
 end
@@ -164,7 +192,7 @@ endmodule
 module ALU_tb;
 
     // Parameters
-    parameter CLK_PERIOD = 10; // Clock period in ns
+    parameter CLK_PERIOD = 100; // Clock period in ns
 
     // Signals
     reg [15:0] regA;
@@ -195,138 +223,149 @@ module ALU_tb;
         // Initialize inputs
         regA = 10;
         regB = 5;
-        // opcode = 4'b0000; // Load operands
+        clk = 0;
         reset = 1;
         #20; // Wait for 20 time units
         reset = 0;
 
         // Test case 1: Addition
-        opcode = 4'b0001; // Add
-        #10;
+        opcode = 4'b0000; // Add
+        #50;
         if (result !== (regA + regB)) begin
-            $display("Addition test failed");
+            $display("Addition test failed", result);
         end else begin
             $display("Addition test passed");
             flag ++;
         end
-
+        #50;
+        
         // Test case 2: Subtraction
         regA = 15;
         regB = 7;
-        opcode = 4'b0010; // Subtract
-        #10;
+        opcode = 4'b0001; // Subtract
+        #50;
         if (result !== (regA - regB)) begin
             $display("Subtraction test failed");
         end else begin
             $display("Subtraction test passed");
             flag ++;
         end
-
+        #50;
+        
         // Test case 3: Multiplication
         regA = 10;
         regB = 3;
-        opcode = 4'b0011; // Multiply
-        #10;
+        opcode = 4'b0010; // Multiply
+        #50;
         if (result !== (regA * regB)) begin
             $display("Multiplication test failed");
         end else begin
             $display("Multiplication test passed");
             flag ++;
         end
-
+        #50;
+        
         // Test case 4: Division
         regA = 25;
         regB = 5;
-        opcode = 4'b0100; // Division
-        #10;
+        opcode = 4'b0011; // Division
+        #50;
         if (result !== (regA / regB)) begin
             $display("Division test failed");
         end else begin
             $display("Division test passed");
             flag ++;
         end
-
+        #50;
+        
         // Test case 5: Bitwise AND
         regA = 15;
         regB = 10;
-        opcode = 4'b0101; // AND
-        #10;
+        opcode = 4'b0100; // AND
+        #50;
         if (result !== (regA & regB)) begin
             $display("Bitwise AND test failed");
         end else begin
             $display("Bitwise AND test passed");
             flag ++;
         end
-
+        #50;
+        
         // Test case 6: Bitwise OR
         regA = 120;
         regB = 224;
-        opcode = 4'b0110; // OR
-        #10;
+        opcode = 4'b0101; // OR
+        #50;
         if (result !== (regA | regB)) begin
             $display("Bitwise OR test failed");
         end else begin
             $display("Bitwise OR test passed");
             flag ++;
         end
-
+        #50;
+        
         // Test case 7: Bitwise XOR
         regA = 134;
         regB = 8;
-        opcode = 4'b0111; // XOR
-        #10;
+        opcode = 4'b0110; // XOR
+        #50;
         if (result !== (regA ^ regB)) begin
             $display("Bitwise XOR test failed");
         end else begin
             $display("Bitwise XOR test passed");
             flag ++;
         end
+        #50;
         
         // Test case 8: Logical Left Shift
         regA = 134;
         regB = 8;
-        opcode = 4'b1000; // LLS
-        #10;
+        opcode = 4'b0111; // LLS
+        #50;
         if (result !== (regA << regB)) begin
             $display("LLS test failed");
         end else begin
             $display("LLS test passed");
             flag ++;
         end
+        #50;
         
         // Test case 9: Logical Right Shift
         regA = 134;
         regB = 8;
-        opcode = 4'b1001; // LRS
-        #10;
+        opcode = 4'b1000; // LRS
+        #50;
         if (result !== (regA >> regB)) begin
             $display("LRS test failed");
         end else begin
             $display("LRS test passed");
             flag ++;
         end
+        #50;
         
         // Test case 10: Increment
         regA = 45;
-        opcode = 4'b1010; // Increment
-        #10;
+        opcode = 4'b1001; // Increment
+        #50;
         if (result !== (regA + 1)) begin
             $display("Increment test failed");
         end else begin
             $display("Increment test passed");
             flag ++;
         end
+        #50;
         
         // Test case 11: Decrement
         regA = 27;
-        opcode = 4'b1011; // Decrement
-        #10;
+        opcode = 4'b1010; // Decrement
+        #50;
         if (result !== (regA - 1)) begin
             $display("Decrement test failed");
         end else begin
             $display("Decrement test passed");
             flag ++;
         end
+        #50;
         
         // End simulation
         if(flag === 11) begin
@@ -337,13 +376,4 @@ module ALU_tb;
         $finish;
     end
 
-endmodule
-
-
-module main;
-  initial 
-    begin
-      $display("Hello, World");
-      $finish ;
-    end
 endmodule
